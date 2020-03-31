5190e32e30487d6bd11c6052367f4cc688aa7a90
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-03-31 16:02:52 -0300

    ViewBinding implementado.

--- app/src/main/java/apps/learn/projetoaps/MainActivity.java
+++ app/src/main/java/apps/learn/projetoaps/MainActivity.java
@@ -1,14 +1,27 @@
 package apps.learn.projetoaps;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import apps.learn.projetoaps.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private ActivityMainBinding activityMainBinding;
+
+    public ActivityMainBinding getActivityMainBinding() {
+        return activityMainBinding;
+    }
+
+    public void setActivityMainBinding(ActivityMainBinding activityMainBinding) {
+        this.activityMainBinding = activityMainBinding;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.setActivityMainBinding(ActivityMainBinding.inflate(getLayoutInflater()));
+        setContentView(this.getActivityMainBinding().getRoot());
     }
+
+
 }
