a5e584e5e5ff97a9fe9593ba76c3c7f2aad7d9ca
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-04-17 17:41:20 -0300

    Contrato de View e Presenter adicionados.

--- app/src/main/java/apps/learn/projetoaps/ui/game_menu/GameMenuContract.java
+++ app/src/main/java/apps/learn/projetoaps/ui/game_menu/GameMenuContract.java
@@ -1,4 +1,19 @@
 package apps.learn.projetoaps.ui.game_menu;
 
+import java.util.Stack;
+
+import apps.learn.projetoaps.data.model.Pergunta;
+
 public interface GameMenuContract {
+
+    interface GameMenuView {
+        void hideStarButton();
+        void hideProgressBar();
+        void showProgressBar();
+    }
+
+    interface GameMenuPresenter {
+        Stack<Pergunta> getPerguntasFromDatabase();
+        void openQuizActivity(Stack<Pergunta> perguntas);
+    }
 }
