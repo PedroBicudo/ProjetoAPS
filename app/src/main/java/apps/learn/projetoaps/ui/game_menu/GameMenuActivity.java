690d335e66ced0e2a4ab455aa76f441fde1c4c25
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-04-17 19:09:20 -0300

    Evento onClick e mudança de tela adicionados.

--- app/src/main/java/apps/learn/projetoaps/ui/game_menu/GameMenuActivity.java
+++ app/src/main/java/apps/learn/projetoaps/ui/game_menu/GameMenuActivity.java
@@ -2,14 +2,20 @@ package apps.learn.projetoaps.ui.game_menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Stack;
+
+import apps.learn.projetoaps.data.model.Pergunta;
 import apps.learn.projetoaps.databinding.ActivityGameMenuBinding;
+import apps.learn.projetoaps.ui.main.MainActivity;
 
 public class GameMenuActivity extends AppCompatActivity implements GameMenuContract.View {
 
     private ActivityGameMenuBinding activityGameMenuBinding;
+    private GameMenuPresenter gameMenuPresenter;
 
     public ActivityGameMenuBinding getActivityGameMenuBinding() {
         return activityGameMenuBinding;
@@ -25,13 +31,13 @@ public class GameMenuActivity extends AppCompatActivity implements GameMenuContr
         activityGameMenuBinding = ActivityGameMenuBinding.inflate(getLayoutInflater());
         setContentView(activityGameMenuBinding.getRoot());
 
-        GameMenuPresenter gameMenuPresenter = new GameMenuPresenter(this);
+        gameMenuPresenter = new GameMenuPresenter(this);
+        activityGameMenuBinding.startGame.setOnClickListener(this);
     }
 
     @Override
     public void hideStartButton() {
         activityGameMenuBinding.startGame.setVisibility(View.GONE);
-
     }
 
     @Override
@@ -39,4 +45,17 @@ public class GameMenuActivity extends AppCompatActivity implements GameMenuContr
         activityGameMenuBinding.progressBar.setVisibility(View.VISIBLE);
     }
 
+    @Override
+    public void openQuizActivity(Stack<Pergunta> perguntas) {
+        Intent intent = new Intent(GameMenuActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        hideStartButton();
+        showProgressBar();
+        Stack<Pergunta> perguntas = gameMenuPresenter.getPerguntasFromDatabase();
+        openQuizActivity(perguntas);
+    }
 }
