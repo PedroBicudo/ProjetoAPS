9adb0192b46dfd8972993f5815374cfc7f8245c0
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-04-18 15:37:22 -0300

    GameMenuContract.Presenter implementado. Métodos da View adicionados em startGameEvent. TODO de DB adicionado.

---
+++ app/src/main/java/apps/learn/projetoaps/ui/game_menu/GameMenuPresenter.java
@@ -0,0 +1,22 @@
+package apps.learn.projetoaps.ui.game_menu;
+
+import java.util.Stack;
+
+import apps.learn.projetoaps.data.model.Pergunta;
+
+public class GameMenuPresenter implements GameMenuContract.Presenter {
+
+    private GameMenuActivity gameMenuActivity;
+
+    public GameMenuPresenter(GameMenuActivity gameMenuActivity) {
+        this.gameMenuActivity = gameMenuActivity;
+    }
+
+    // TODO - Adicionar método Util para obter perguntas do banco de dados
+    @Override
+    public void startGameEvent() {
+        gameMenuActivity.disableStartGameButton();
+        gameMenuActivity.showProgressBar();
+        gameMenuActivity.openQuizActivity(new Stack<Pergunta>());
+    }
+}
