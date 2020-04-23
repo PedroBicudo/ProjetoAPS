02e5a15e9251343de9fc93d0f5d24f0578927461
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-04-23 14:39:15 -0300

    Contrato de Presenter implementado.

--- app/src/main/java/apps/learn/projetoaps/ui/splash_screen/SplashScreenPresenter.java
+++ app/src/main/java/apps/learn/projetoaps/ui/splash_screen/SplashScreenPresenter.java
@@ -1,4 +1,23 @@
 package apps.learn.projetoaps.ui.splash_screen;
 
-public class SplashScreenPresenter {
+import android.os.Handler;
+
+public class SplashScreenPresenter implements SplashScreenContract.Presenter{
+
+    private SplashScreen splashScreen;
+
+    public SplashScreenPresenter(SplashScreen splashScreen) {
+        this.splashScreen = splashScreen;
+    }
+
+    @Override
+    public void waitAndChangeUi() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                splashScreen.openGameMenuActivity();
+            }
+        }, DEFAULT_WAIT_SECONDS);
+    }
 }
