bcfe66e3b3fcfe85c60abbed7997a8ad2113b803
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-04-23 14:38:22 -0300

    Contrato de View implementado.

--- app/src/main/java/apps/learn/projetoaps/ui/splash_screen/SplashScreen.java
+++ app/src/main/java/apps/learn/projetoaps/ui/splash_screen/SplashScreen.java
@@ -2,13 +2,31 @@ package apps.learn.projetoaps.ui.splash_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+import apps.learn.projetoaps.R;
+import apps.learn.projetoaps.databinding.ActivitySplashScreenBinding;
+import apps.learn.projetoaps.ui.game_menu.GameMenuActivity;
+
+public class SplashScreen extends AppCompatActivity implements SplashScreenContract.View{
+
+    ActivitySplashScreenBinding activitySplashScreenBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        activitySplashScreenBinding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(activitySplashScreenBinding.getRoot());
+
+        SplashScreenPresenter splashScreenPresenter = new SplashScreenPresenter(SplashScreen.this);
+        splashScreenPresenter.waitAndChangeUi();
+    }
+
+    @Override
+    public void openGameMenuActivity() {
+        Intent intent = new Intent(SplashScreen.this, GameMenuActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
