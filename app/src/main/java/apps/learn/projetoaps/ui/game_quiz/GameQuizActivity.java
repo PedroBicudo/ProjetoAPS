5732d4c83d9b7a70170a526f435af0b7bcf7d577
Peter <43938917+PedroBicudo@users.noreply.github.com>
2020-05-09 15:57:50 -0300

    Contrato View implementado e vínculo com o presenter efetuado.  (#17)
    * Contrato View implementado.
    
    * View vinculada ao presenter.
    
    Co-authored-by: Pedro Henrique <pedroabicudo_2012@hotmail.com>

--- app/src/main/java/apps/learn/projetoaps/ui/game_quiz/GameQuizActivity.java
+++ app/src/main/java/apps/learn/projetoaps/ui/game_quiz/GameQuizActivity.java
@@ -4,16 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
+import apps.learn.projetoaps.data.model.Alternativa;
+import apps.learn.projetoaps.data.model.Pergunta;
 import apps.learn.projetoaps.databinding.ActivityGameQuizBinding;
 
-public class GameQuizActivity extends AppCompatActivity {
+public class GameQuizActivity extends AppCompatActivity implements GameQuizContract.View {
 
     private static final String TAG = "GameQuizActivity";
     private ActivityGameQuizBinding activityGameQuizBinding;
+    private GameQuizPresenter gameQuizPresenter;
+
+    public GameQuizPresenter getGameQuizPresenter() {
+        return gameQuizPresenter;
+    }
+
+    public void setGameQuizPresenter(GameQuizPresenter gameQuizPresenter) {
+        this.gameQuizPresenter = gameQuizPresenter;
+    }
 
     public ActivityGameQuizBinding getActivityGameQuizBinding() {
         return activityGameQuizBinding;
@@ -28,7 +43,51 @@ public class GameQuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.setActivityGameQuizBinding(ActivityGameQuizBinding.inflate(getLayoutInflater()));
         setContentView(this.getActivityGameQuizBinding().getRoot());
+        Stack<Pergunta> perguntas = new Stack<>();
+
+        this.setGameQuizPresenter(new GameQuizPresenter(this, perguntas));
+    }
+
+    @Override
+    public void changePergunta(String title) {
+        this.getActivityGameQuizBinding().question.setText(title);
+    }
+
+    @Override
+    public void changeAlternativas(Alternativa[] alternativas) {
+        ArrayList<View> views = this.getActivityGameQuizBinding().choices.getTouchables();
+
+        for (int pos=0; pos < views.size(); pos++) {
+            Button choice = (Button) views.get(pos);
+            Alternativa alternativa = alternativas[pos];
+            choice.setText(alternativa.getTexto());
+            choice.setTag(choice.getId(), alternativa);
+        }
+    }
+
+    @Override
+    public void changeButtonColor(Button button, String color) {
+        button.setBackgroundColor(Color.parseColor(color));
+    }
+
+    @Override
+    public void resetButtonsColor() {
+        Button defaultButton = new Button(this);
+        ArrayList<View> views = this.getActivityGameQuizBinding().choices.getTouchables();
+
+        for (View view: views) {
+            Button choice = (Button) view;
+            choice.setBackground(defaultButton.getBackground());
+        }
+    }
+
+    @Override
+    public void openScoreActivity(int score) {
 
     }
 
+    @Override
+    public void onClick(View v) {
+        this.getGameQuizPresenter().verificarEscolhaEMudarPergunta((Button) v);
+    }
 }
