15b949af082157780c10277a0c3303b4fecba553
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-05-09 15:03:32 -0300

    Pontuação adicionada.

--- app/src/main/java/apps/learn/projetoaps/ui/game_quiz/GameQuizPresenter.java
+++ app/src/main/java/apps/learn/projetoaps/ui/game_quiz/GameQuizPresenter.java
@@ -3,16 +3,20 @@ package apps.learn.projetoaps.ui.game_quiz;
 import android.util.Log;
 import android.widget.Button;
 
+import java.util.Stack;
+
 import apps.learn.projetoaps.data.model.Alternativa;
+import apps.learn.projetoaps.data.model.Pergunta;
 
 public class GameQuizPresenter implements GameQuizContract.Presenter {
 
     private GameQuizActivity gameQuizActivity;
+    private int score;
 
     public GameQuizPresenter(GameQuizActivity gameQuizActivity) {
         this.gameQuizActivity = gameQuizActivity;
+        this.score = 0;
     }
-
 
     @Override
     public void changeCurrentPergunta() {}
@@ -20,7 +24,15 @@ public class GameQuizPresenter implements GameQuizContract.Presenter {
     @Override
     public void isAlternativaCorreta(Button button) {
         Alternativa alternativa = (Alternativa) button.getTag(button.getId());
-        String color = alternativa.getCorreta()? "green": "red";
+        String color;
+        if (alternativa.getCorreta()) {
+            score++;
+            color = "green";
+
+        } else {
+            color = "red";
+
+        }
         this.gameQuizActivity.changeButtonColor(button, color);
     }
 
