e778b8aecd0bc617b75d292d408592aea37b24c6
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-05-09 12:41:20 -0300

    Método de mudança de alternativas adicionado.

--- app/src/main/java/apps/learn/projetoaps/ui/game_quiz/GameQuizContract.java
+++ app/src/main/java/apps/learn/projetoaps/ui/game_quiz/GameQuizContract.java
@@ -1,9 +1,12 @@
 package apps.learn.projetoaps.ui.game_quiz;
 
+import apps.learn.projetoaps.data.model.Alternativa;
+
 public interface GameQuizContract {
 
     interface View {
         void changePergunta(String title);
+        void changeAlternativas(Alternativa[] alternativas);
     }
 
     interface Presenter {}
