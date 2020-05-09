798afea1458126a9a42602b329a0b1b776f5c691
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-05-09 12:40:10 -0300

    Contrato View reiniciado.

--- app/src/main/java/apps/learn/projetoaps/ui/game_quiz/GameQuizContract.java
+++ app/src/main/java/apps/learn/projetoaps/ui/game_quiz/GameQuizContract.java
@@ -1,15 +1,8 @@
 package apps.learn.projetoaps.ui.game_quiz;
 
-import android.widget.Button;
-
-import apps.learn.projetoaps.data.model.Alternativa;
-
 public interface GameQuizContract {
 
-    interface View {
-        void changeQuestionWording(String title);
-        void changeChoiceButton(Button button, Alternativa alternativa);
-    }
+    interface View {}
 
     interface Presenter {}
 }
