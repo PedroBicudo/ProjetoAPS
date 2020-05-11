7da1e9d30a433b096f7d0249ac13f759c9e7769e
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-05-11 19:13:57 -0300

    Modelos transformados em entidades.

--- app/src/main/java/apps/learn/projetoaps/data/model/Pergunta.java
+++ app/src/main/java/apps/learn/projetoaps/data/model/Pergunta.java
@@ -1,12 +1,28 @@
 package apps.learn.projetoaps.data.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import io.reactivex.annotations.NonNull;
+
+@Entity(tableName = "Questoes")
 public class Pergunta {
+
+    @NonNull
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "ID_Questoes")
     private int idPergunta;
+
+    @ColumnInfo(name = "Perguntas")
     private String enunciado;
+
+    @Ignore
     private Alternativa alternativas[] = new Alternativa[4];
 
     public void setIdPergunta(int idPergunta){ this.idPergunta = idPergunta; }
