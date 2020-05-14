b84f7a9bc6aa73b11104030eb06b30554afdee1c
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-05-14 14:20:47 -0300

    Activity GameScore adicionada.

---
+++ app/src/main/java/apps/learn/projetoaps/ui/game_score/GameScoreActivity.java
@@ -0,0 +1,16 @@
+package apps.learn.projetoaps.ui.game_score;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import apps.learn.projetoaps.R;
+
+public class GameScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_score);
+    }
+}
