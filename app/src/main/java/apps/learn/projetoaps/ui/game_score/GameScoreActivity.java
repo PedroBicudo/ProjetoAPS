cea29f168b2aa4b4b5ab89a98ea7777e70247448
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-05-14 22:01:13 -0300

    Contrato atualizado e algoritmos implementados.

--- app/src/main/java/apps/learn/projetoaps/ui/game_score/GameScoreActivity.java
+++ app/src/main/java/apps/learn/projetoaps/ui/game_score/GameScoreActivity.java
@@ -1,21 +1,25 @@
 package apps.learn.projetoaps.ui.game_score;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import java.util.List;
 import java.util.Stack;
-
 import apps.learn.projetoaps.R;
-import apps.learn.projetoaps.data.model.Jogador;
 import apps.learn.projetoaps.data.model.Pergunta;
+import apps.learn.projetoaps.databinding.ActivityGameScoreBinding;
+import apps.learn.projetoaps.ui.adapter.PlayersViewAdapter;
+import apps.learn.projetoaps.ui.game_quiz.GameQuizActivity;
 
 public class GameScoreActivity extends AppCompatActivity implements GameScoreContract.View {
 
+    private ActivityGameScoreBinding activityGameScoreBinding;
     private static final String TAG = "GameScoreActivity";
     private GameScorePresenter gameScorePresenter;
     private View view;
@@ -23,10 +27,21 @@ public class GameScoreActivity extends AppCompatActivity implements GameScoreCon
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.view = getLayoutInflater().inflate(R.layout.fragment_insert_player_name, null);
+        activityGameScoreBinding = ActivityGameScoreBinding.inflate(getLayoutInflater());
+        setContentView(activityGameScoreBinding.getRoot());
+
         this.gameScorePresenter = new GameScorePresenter(GameScoreActivity.this);
+        this.gameScorePresenter.loadPlayerList();
+
+        this.view = getLayoutInflater().inflate(R.layout.fragment_insert_player_name, null);
         getAlertDialog().show();
 
+        this.activityGameScoreBinding.btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gameScorePresenter.loadQuizQuestions();
+            }
+        });
     }
 
     @Override
@@ -37,7 +52,7 @@ public class GameScoreActivity extends AppCompatActivity implements GameScoreCon
                 .setPositiveButton("Adicionar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        gameScorePresenter.adicionarNovoJogador();
+                        gameScorePresenter.addNewPlayer();
                     }
                 })
                 .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
@@ -55,12 +70,28 @@ public class GameScoreActivity extends AppCompatActivity implements GameScoreCon
     }
 
     @Override
-    public void populateRecyclerView(List<Jogador> jogadores) {
-
+    public void setProgressBarGone() {
+        this.activityGameScoreBinding.progressBar.setVisibility(ProgressBar.GONE);
     }
 
     @Override
-    public void openGameQuizActivity(Stack<Pergunta> perguntas) {}
+    public void setProgressBarVisible() {
+        this.activityGameScoreBinding.progressBar.setVisibility(ProgressBar.VISIBLE);
+    }
 
+    @Override
+    public void populateRecyclerView(PlayersViewAdapter playersViewAdapter) {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        this.activityGameScoreBinding.listPlayersRank.setAdapter(playersViewAdapter);
+        this.activityGameScoreBinding.listPlayersRank.setLayoutManager(layoutManager);
+    }
+
+    @Override
+    public void openGameQuizActivity(Stack<Pergunta> perguntas) {
+        Intent intent = new Intent(GameScoreActivity.this, GameQuizActivity.class);
+        intent.putExtra("quizQuestions", perguntas);
+        startActivity(intent);
+        finish();
+    }
 
 }
